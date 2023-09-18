d4299808e0d323deb42f276e4728564ba2355146
SarvarSamigov <samigovsarvar7@gmail.com>
2023-09-18 09:17:24 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Layout,Menu} from 'antd';
+import { Header,Content  } from 'antd/es/layout/layout';
+import Sider from 'antd/es/layout/Sider';
+
+import { useState } from 'react';
+
+import {AiOutlineHome} from 'react-icons/ai'
+import {GrOrganization} from 'react-icons/gr'
+import {BsPersonSquare} from "react-icons/bs"
+import {GiHamburgerMenu} from "react-icons/gi"
+import {FiLogIn} from "react-icons/fi"
+import {HiOutlineNewspaper} from "react-icons/hi2"
+
+
+
+import './styles/dashboard.css'
+
+
 
 function App() {
+ const [collpased,setCollpased] = useState(false)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   
+   <Layout className='container'>
+      <Header className='header'>
+      
+      <div style={{display: "flex",alignItems:"center"}} >
+        <GiHamburgerMenu onClick={()=>{
+          setCollpased(!collpased)
+        }} size={28} style={{marginRight : 20}}/>
+        <div className="brand">Admin Panel </div>
+      
+      </div>
+      
+      </Header>
+      
+      
+      <Layout>
+      <Sider className='sider' collapsed={collpased} theme='light'>
+        <Menu style={{fontSize:15,borderRadius:20}} 
+        mode='inline'
+        items={[
+            {
+              key: 'home',
+              icon:  <AiOutlineHome/>,
+              label: 'Home',
+              children: [{
+                label : "All Users",
+                key : "all_users",
+                icon: <BsPersonSquare/>
+              }]
+            },
+            {
+              key: 'about_us',
+              icon:  <GrOrganization/>,
+              label: 'About Us',
+            },
+            {key: 'login',
+              icon:  <FiLogIn/>,
+              label: 'Login'
+            },
+            {key: 'register',
+              icon:  <HiOutlineNewspaper/>,
+              label: 'Register'
+          }
+          ]} />  
+
+       </Sider>
+
+      <Content>
+          
+      </Content>
+      </Layout>
+    </Layout>
+
+  )
 }
 
-export default App;
+export default App
